bf1b9c85d91da2d454092edb4ecffaba1cdcde4c
pedro <pedromf34@outlook.com>
2020-11-22 15:57:09 -0300

    Commit inicial

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "plugin:react/recommended",
+        "airbnb",
+        "prettier",
+        "prettier/react",
+    ],
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["react", "prettier", "react-hooks"],
+    rules: {
+        "prettier/prettier": "error",
+        "import/prefer-default-export": 0,
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+    },
+};
